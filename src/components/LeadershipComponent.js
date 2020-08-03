d5180ca77938be22211c4180baefbdc04e032584
Enrique Shunnar <xtealer00@gmail.com>
2020-08-03 09:50:51 -0500

    update text on Leadership component

--- src/components/LeadershipComponent.js
+++ src/components/LeadershipComponent.js
@@ -3,15 +3,15 @@ import React from "react";
 const LeadershipComponent = (props) => (
   <div className="section-component leadership-component" id="leadership">
     <div className="left-container">
-      <h2>ALWAYS TEAMWORK</h2>
+      <h2>Leadership</h2>
     </div>
 
     <div className="center-container" />
 
     <div className="right-container">
       <p>
-        We believe that a good team is key to achieve success in the making of
-        every project.
+        We believe that a good team leader is key to achieve success in the
+        making of every project, always promoting teamwork.
       </p>
     </div>
   </div>
